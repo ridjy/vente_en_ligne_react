a67ea076b388e6db6f599516a4e43a7a54a1b355
Eddy <eddy@qdata-mg.com>
2021-12-10 09:03:43 +0300

    ajout evenement

--- src/composants/App.js
+++ src/composants/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 import Banner from './Banner';
 import Cart from './Cart';
+import Footer from './Footer';
+import logo from '../assets/logo.png';
 import ShoppingList from './ShoppingList';
 
 function App() {
   return (
     <div>
-      <Banner /><Cart /><ShoppingList />
+      <Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+      <Cart /><ShoppingList />
+      <Footer />
     </div>  
   );
 }
--- src/composants/Banner.js
+++ src/composants/Banner.js
@@ -1,12 +1,7 @@
 import '../styles/Banner.css'
-import logo from '../assets/logo.png'
 
-function Banner() {
-    const title = 'La maison jungle';//test
-    return (<div className='lmj-banner' /*style={{ color : 'blue', textAlign : 'left', fontSize : 15 }}*/>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1 className='lmj-title'>{title}</h1>
-</div>)
-} 
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
+}
 
 export default Banner
