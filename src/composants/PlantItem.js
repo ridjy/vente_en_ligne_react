8b2b1f78dc307795ef1adc7efe6d00e875a5ef99
Eddy <eddy@qdata-mg.com>
2022-06-17 12:27:47 +0300

    coorection key

--- src/composants/PlantItem.js
+++ src/composants/PlantItem.js
@@ -3,7 +3,7 @@ import '../styles/PlantItem.css'
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={`${name}-${id}`} id={`${name}-${id}`} className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li key={id} id={`${name}-${id}`} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
--- src/composants/ShoppingList.js
+++ src/composants/ShoppingList.js
@@ -19,7 +19,8 @@ function ShoppingList() {
 			<ul className='lmj-plant-list'>
 				{plantList.map(({ id, cover, name, water, light }) => (
 					<PlantItem
-						id={id}
+						key={id}
+						id = {id}
 						cover={cover}
 						name={name}
 						water={water}
