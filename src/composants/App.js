b5576481f219b0729da089ee191b37997942cb9f
Eddy <eddy@qdata-mg.com>
2022-06-17 15:08:46 +0300

    Ajout details dans le panier

--- src/composants/App.js
+++ src/composants/App.js
@@ -4,21 +4,24 @@ import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
+import { useState } from 'react'
 
 function App() {
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
-			</div>
-			<Footer />
-		</div>
-	)
+  const [cart, updateCart] = useState([])
+  
+  return (
+      <div>
+          <Banner>
+              <img src={logo} alt='La maison jungle' className='lmj-logo' />
+              <h1 className='lmj-title'>La maison jungle</h1>
+          </Banner>
+          <div className='lmj-layout-inner'>
+              <Cart cart={cart} updateCart={updateCart} />
+              <ShoppingList cart={cart} updateCart={updateCart} />
+          </div>
+          <Footer />
+      </div>
+  )
 }
 
 export default App
--- src/composants/PlantItem.js
+++ src/composants/PlantItem.js
@@ -1,9 +1,15 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light, price }) 
+{
 	return (
-		<li key={id} id={`${name}-${id}`} className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -12,11 +18,6 @@ function PlantItem({ id, cover, name, water, light }) {
 			</div>
 		</li>
 	)
-}
-
-/*évènement*/
-function handleClick(plantName) {
-    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
 }
 
 export default PlantItem
